fc40b84e0e86e3c6c554e900425989cc5a078ca2
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-05-31 00:49:36 +0900

    상품 폴더 설계

---
+++ src/main/java/com/sparta/myselectshop/entity/Folder.java
@@ -0,0 +1,32 @@
+package com.sparta.myselectshop.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "folder")
+public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    //항상 회원의 정보를 가져올 필요가 없어서
+    //필요할 때 회원의 정보를 조회
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Folder(String name, User user) {
+        this.name = name;
+        this.user = user;
+    }
+}
